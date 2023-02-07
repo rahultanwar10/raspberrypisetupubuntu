beab547034966c5a98c81c3e9c4fe9399fa6d6e1
rahultanwar10 <rahultanwarpec@gmail.com>
2023-02-07 16:36:34 +0530

    Version 1

--- installRealSense.sh
+++ installRealSense.sh
@@ -1,18 +1,48 @@
-sudo apt-get install -y libdrm-amdgpu1 libdrm-amdgpu1-dbgsym libdrm-dev libdrm-exynos1 libdrm-exynos1-dbgsym libdrm-freedreno1 libdrm-freedreno1-dbgsym libdrm-nouveau2 libdrm-nouveau2-dbgsym libdrm-omap1 libdrm-omap1-dbgsym libdrm-radeon1 libdrm-radeon1-dbgsym libdrm-tegra0 libdrm-tegra0-dbgsym libdrm2 libdrm2-dbgsym
-sudo apt-get install -y libglu1-mesa libglu1-mesa-dev glusterfs-common libglu1-mesa libglu1-mesa-dev libglui-dev libglui2c2
-sudo apt-get install -y libglu1-mesa libglu1-mesa-dev mesa-utils mesa-utils-extra xorg-dev libgtk-3-dev libusb-1.0-0-dev
+sudo apt-get update && sudo apt-get dist-upgrade
+sudo apt-get install automake libtool vim cmake libusb-1.0-0-dev libx11-dev xorg-dev libglu1-mesa-dev libssl-dev libtbb-dev
 
-# Installing TBB
-cd ~/Desktop
-mkdir tbb && cd tbb
-wget https://github.com/PINTO0309/TBBonARMv7/raw/master/libtbb-dev_2018U2_armhf.deb
-sudo dpkg -i libtbb-dev_2018U2_armhf.deb
-sudo ldconfig
+cd ~
+git clone https://github.com/IntelRealSense/librealsense.git
+cd librealsense
+sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/
+sudo su << EOF
+udevadm control --reload-rules && udevadm trigger
+exit
+EOF
 
-# Installing opencv
-sudo apt -y autoremove libopencv3
-cd ~/Desktop
-mkdir opencv && cd opencv
-wget https://github.com/mt08xx/files/raw/master/opencv-rpi/libopencv3_3.4.3-20180907.1_armhf.deb
-sudo apt install -y ./libopencv3_3.4.3-20180907.1_armhf.deb
+echo 'export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+source ~/.bashrc
+
+wget https://github.com/protocolbuffers/protobuf/releases/download/v21.12/protobuf-all-21.12.tar.gz
+tar -xzvf protobuf-all-21.12.tar.gz
+cd protobuf-all-21.12
+./configure
+make -j4
+sudo make install
+cd python
+export LD_LIBRARY_PATH=../src/.libs
+python3 setup.py build --cpp_implementation 
+python3 setup.py test --cpp_implementation
+sudo python3 setup.py install --cpp_implementation
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=3
 sudo ldconfig
+protoc --version
+
+cd ~
+cd librealsense
+mkdir  build  && cd build
+cmake .. -DBUILD_EXAMPLES=true -DCMAKE_BUILD_TYPE=Release -DFORCE_LIBUVC=true
+make -j1
+sudo make install
+
+cmake .. -DBUILD_PYTHON_BINDINGS=bool:true -DPYTHON_EXECUTABLE=$(which python3)
+make -j4
+sudo make install
+
+echo 'export PYTHONPATH=$PYTHONPATH:/usr/local/lib' >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt-get install python3-opengl
+sudo -H pip3 install pyopengl
+sudo pip3 install git+https://github.com/mcfletch/pyopengl.git@227f9c66976d9f5dadf62b9a97e6beaec84831ca#subdirectory=accelerate
